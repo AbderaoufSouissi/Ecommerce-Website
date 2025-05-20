4da9c7e3e52c77437febfc1a546a64acda579e37
abderaouf souissi <abderaouf.souissi@outlook.com>
2025-05-20 18:56:36 +0100

    frontend: remove black border from Card component

--- ecomm-client/src/components/Card/Card.tsx
+++ ecomm-client/src/components/Card/Card.tsx
@@ -10,7 +10,7 @@ const Card = ({imagePath,title,alt}:CardProps) => {
         <div className="flex flex-col p-6">
             <img
                 src={imagePath}
-                className="h-[240px] w-[180px] bg-cover bg-center border rounded hover:scale-105 cursor-pointer transition duration-300 ease-in-out "
+                className="h-[240px] w-[180px] bg-cover bg-center rounded hover:scale-105 cursor-pointer transition duration-300 ease-in-out"
                 alt={alt}
             />
 
