0e2423ea095e29ad21703a42bed12362c5846b7c
abderaouf souissi <abderaouf.souissi@outlook.com>
2025-05-30 16:21:18 +0100

    backend: add repository for Category and Product Entities

---
+++ ecomm-api/src/main/java/com/ars/ecomm_api/repository/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.ars.ecomm_api.repository;
+
+import com.ars.ecomm_api.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, UUID> {
+}
---
+++ ecomm-api/src/main/java/com/ars/ecomm_api/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.ars.ecomm_api.repository;
+
+import com.ars.ecomm_api.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+@Repository
+public interface ProductRepository extends JpaRepository<Product, UUID> {
+}
