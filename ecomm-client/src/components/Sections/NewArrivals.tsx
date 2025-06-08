3ad22355ea33ffcddb2219215b40da341fa3fe16
abderaouf souissi <abderaouf.souissi@outlook.com>
2025-06-08 15:22:00 +0100

    frontend: improved loading speed of sections in home page

--- ecomm-client/src/components/Sections/NewArrivals.tsx
+++ ecomm-client/src/components/Sections/NewArrivals.tsx
@@ -1,10 +1,10 @@
+import { useMemo } from "react";
 import Card, { type CardProps } from "../Card/Card";
 import SectionHeading from "./SectionsHeading/SectionHeading";
-
-
 import Carousel from "react-multi-carousel";
 import { responsive } from "../../utils/Section.constants";
 import "./NewArrivals.css";
+
 import dress_shirts from '../../assets/img/arrivals-dress-shirts.jpg';
 import jeans from '../../assets/img/arrivals-jeans.jpg';
 import mannequin from '../../assets/img/arrivals-mannequin.jpg';
@@ -12,18 +12,24 @@ import shirts from '../../assets/img/arrivals-shirts.jpg';
 import lumberjack from '../../assets/img/arrivals-lumberjack.jpg';
 import joggers from '../../assets/img/arrivals-joggers.jpg';
 
-
 const items: CardProps[] = [
-  { title: "", imagePath:dress_shirts },
-  { title: "", imagePath:jeans },
+  { title: "", imagePath: dress_shirts },
+  { title: "", imagePath: jeans },
   { title: "", imagePath: mannequin },
   { title: "", imagePath: shirts },
   { title: "", imagePath: lumberjack },
   { title: "", imagePath: joggers },
 ];
 
-
 const NewArrivals = () => {
+  const memoCards = useMemo(
+    () =>
+      items.map((item, index) => (
+        <Card key={index} imagePath={item.imagePath} title={item.title} />
+      )),
+    []
+  );
+
   return (
     <>
       <SectionHeading title="New Arrivals" />
@@ -38,14 +44,7 @@ const NewArrivals = () => {
         itemClass={"react-slider-custom-item"}
         className="px-8"
       >
-        {items &&
-          items.map((item, index) => (
-            <Card
-              key={index}
-              imagePath={item.imagePath}
-              title={item.title}
-            />
-          ))}
+        {memoCards}
       </Carousel>
     </>
   );
