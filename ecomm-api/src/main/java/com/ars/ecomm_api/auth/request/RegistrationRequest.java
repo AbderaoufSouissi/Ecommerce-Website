ba6ad18a31037be3082d05975551013e32d54b82
abderaouf souissi <abderaouf.souissi@outlook.com>
2025-06-09 11:21:57 +0100

    backend: add login request and registration request and response

---
+++ ecomm-api/src/main/java/com/ars/ecomm_api/auth/request/RegistrationRequest.java
@@ -0,0 +1,19 @@
+package com.ars.ecomm_api.auth.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RegistrationRequest {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private CharSequence password;
+    private String phoneNumber;
+
+}
---
+++ ecomm-api/src/main/java/com/ars/ecomm_api/auth/request/RegistrationResponse.java
@@ -0,0 +1,16 @@
+package com.ars.ecomm_api.auth.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RegistrationResponse {
+    private int code;
+    private String message;
+
+}
